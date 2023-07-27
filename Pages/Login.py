f057e09e20ba155e0be9e645a11f9b309ed6ac58
saimohanb <saimohan.bandaru@gmail.com>
2023-07-27 14:35:17 +0530

    added a folder Support

--- Pages/Login.py
+++ Pages/Login.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from lib import Helper
+from Support.lib import Helper
 
 
 class LoginPage:

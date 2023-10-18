ad06db50050c31912ccf0b94d7b2c4049e528487
saimohanb <saimohan.bandaru@gmail.com>
2023-10-18 23:00:25 +0530

    Modified in Pycharm

--- Pages/Login.py
+++ Pages/Login.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from Support.lib import Helper
+from saucedemo.Support.lib import Helper
 
 
 class LoginPage:
